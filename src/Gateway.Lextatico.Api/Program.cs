4b295b0150f7cc4dd22bdaa048aefdf73f7e53c0
Cassiano Raupp <cassiano.raupp@outlook.com>
2022-03-14 16:23:22 -0300

    Commit inicial.

---
+++ src/Gateway.Lextatico.Api/Program.cs
@@ -0,0 +1,64 @@
+using Newtonsoft.Json;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureAppConfiguration(ic =>
+{
+    ic.AddJsonFile("configuration.json");
+    ic.AddJsonFile($"configuration.{builder.Environment.EnvironmentName}.json", true);
+});
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+if (!app.Environment.IsProduction())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+if (app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+
+    app.UseHsts();
+}
+
+var conf = new OcelotPipelineConfiguration()
+{
+    PreErrorResponderMiddleware = async (ctx, next) =>
+    {
+        var dateTimeNow = DateTime.Now;
+
+        if (ctx.Request.Path.Equals(new PathString("/healthchecks-data-ui")))
+        {
+            await ctx.Response.WriteAsync(JsonConvert.SerializeObject(new
+            {
+                status = "Healthy",
+                totalDuration = DateTime.Now - dateTimeNow,
+                entries = new
+                {
+                    API = new
+                    {
+                        data = new { },
+                        description = "Gateway up!",
+                        duration = DateTime.Now - dateTimeNow,
+                        status = "Healthy",
+                        tags = Array.Empty<string>()
+                    }
+                }
+            }));
+        }
+        else
+        {
+            await next.Invoke();
+        }
+    }
+};
+app.UseOcelot(conf).Wait();
+
+app.Run();
