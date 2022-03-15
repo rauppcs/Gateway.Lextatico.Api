298dccb1937fbf1422f23382ff197fb5d1ade43e
Cassiano Raupp <cassiano.raupp@outlook.com>
2022-03-15 16:05:33 -0300

    Add swagger for gateway.

--- src/Gateway.Lextatico.Api/Program.cs
+++ src/Gateway.Lextatico.Api/Program.cs
@@ -1,14 +1,23 @@
+using System.Net.Mime;
 using Gateway.Lextatico.Api.Configurations;
+using Gateway.Lextatico.Api.Extensions;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Newtonsoft.Json;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using HostEnvironmentEnvExtensions = Gateway.Lextatico.Api.Extensions.HostEnvironmentEnvExtensions;
+
+if (HostEnvironmentEnvExtensions.IsDocker())
+    Thread.Sleep(30000);
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureAppConfiguration((hostContext, builder) =>
 {
-    if (hostContext.HostingEnvironment.EnvironmentName == "LocalDevelopment")
-        builder.AddUserSecrets<Program>();
+    // if (hostContext.HostingEnvironment.IsLocalDevelopment())
+    builder.AddUserSecrets<Program>();
 });
 
 builder.WebHost.ConfigureAppConfiguration(ic =>
@@ -20,6 +29,11 @@ builder.WebHost.ConfigureAppConfiguration(ic =>
 builder.Services.AddLextaticoJwt(builder.Configuration);
 
 builder.Services.AddLexitaticoCors();
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Services.AddHealthChecks()
+    .AddCheck("API", () => new HealthCheckResult(HealthStatus.Healthy, "Gatway is up!"));
 
 builder.Services.AddOcelot(builder.Configuration);
 
@@ -39,37 +53,7 @@ if (app.Environment.IsProduction())
     app.UseHsts();
 }
 
-var conf = new OcelotPipelineConfiguration()
-{
-    PreErrorResponderMiddleware = async (ctx, next) =>
-    {
-        var dateTimeNow = DateTime.Now;
-
-        if (ctx.Request.Path.Equals(new PathString("/healthchecks-data-ui")))
-        {
-            await ctx.Response.WriteAsync(JsonConvert.SerializeObject(new
-            {
-                status = "Healthy",
-                totalDuration = DateTime.Now - dateTimeNow,
-                entries = new
-                {
-                    API = new
-                    {
-                        data = new { },
-                        description = "Gateway up!",
-                        duration = DateTime.Now - dateTimeNow,
-                        status = "Healthy",
-                        tags = Array.Empty<string>()
-                    }
-                }
-            }));
-        }
-        else
-        {
-            await next.Invoke();
-        }
-    }
-};
+app.UseRouting();
 
 app.UseCors();
 
@@ -77,6 +61,38 @@ app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.UseOcelot(conf).Wait();
+app.UseEndpoints(cf =>
+{
+    cf.MapHealthChecks("/status",
+              new HealthCheckOptions()
+              {
+                  ResponseWriter = async (context, report) =>
+                  {
+                      var result = JsonConvert.SerializeObject(
+                          new
+                          {
+                              statusApplication = report.Status.ToString(),
+                              healthChecks = report.Entries.Select(e => new
+                              {
+                                  check = e.Key,
+                                  ErrorMessage = e.Value.Exception?.Message,
+                                  status = Enum.GetName(typeof(HealthStatus), e.Value.Status)
+                              })
+                          });
+                      context.Response.ContentType = MediaTypeNames.Application.Json;
+                      await context.Response.WriteAsync(result);
+                  }
+              });
+
+    cf.MapHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+});
+
+app.UseSwaggerForOcelotUI();
+
+app.UseOcelot().Wait();
 
 app.Run();
